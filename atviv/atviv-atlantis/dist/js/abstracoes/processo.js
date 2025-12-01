d32123798afb2ca25c2fc2fab2e9d7feb53909ff
PedHr <pedrohe313131@gmail.com>
2025-12-01 00:29:45 -0300

    atvii adicionada

--- atviv/atviv-atlantis/dist/js/abstracoes/processo.js
+++
@@ -1,9 +0,0 @@
-import Entrada from "../io/entrada.js";
-export default class Processo {
-    constructor() {
-        this.entrada = new Entrada();
-    }
-    get Execucao() {
-        return this.execucao;
-    }
-}
