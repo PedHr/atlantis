38c7f186896cdd617e03db3552869c839771ad11
PedHr <pedrohe313131@gmail.com>
2025-12-01 00:31:22 -0300

    atviii adicionada

--- atviv/atviv-atlantis/dist/js/enumeracoes/TipoDocumento.js
+++
@@ -1,6 +0,0 @@
-export var TipoDocumento;
-(function (TipoDocumento) {
-    TipoDocumento["CPF"] = "Cadastro de Pessoas F\u00EDsica";
-    TipoDocumento["RG"] = "Registro Geral";
-    TipoDocumento["Passaporte"] = "Passaporte";
-})(TipoDocumento || (TipoDocumento = {}));
