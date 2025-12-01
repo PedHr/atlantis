53f84b9b508eb4d067e20584ad881682401f1ce6
PedHr <pedrohe313131@gmail.com>
2025-12-01 00:33:38 -0300

    Atividades removidas da main

--- atviv/atviv-atlantis/dist/js/modelos/hospedagem.js
+++
@@ -1,8 +0,0 @@
-export default class Hospedagem {
-    constructor(titular, acomodacao) {
-        this.titular = titular;
-        this.acomodacao = acomodacao;
-    }
-    get Titular() { return this.titular; }
-    get Acomodacao() { return this.acomodacao; }
-}
